387b6389f688455e068e22b6ec70fb289c69386b
Chi Rilo <cbenolirao@ibsportal.com.au>
2025-04-24 19:56:47 +0800

    import local fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                //sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['RethinkSansBold', 'RethinkSansBoldItalic', 'RethinkSansExtraBold', 'RethinkSansExtraBoldItalic', 'RethinkSansItalic', 'RethinkSansMedium', 'RethinkSansMediumItalic', 'RethinkSansRegular', 'RethinkSansSemiBold', 'RethinkSansSemiBoldItalic', ...defaultTheme.fontFamily.sans],
             },
         },
     },
