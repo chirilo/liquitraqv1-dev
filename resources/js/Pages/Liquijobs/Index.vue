b84d9164a178c783f1f80b60fe70fbc4b8f80b20
Chi Rilo <cbenolirao@ibsportal.com.au>
2025-05-12 15:49:45 +0800

    added link to My Account

--- resources/js/Pages/Liquijobs/Index.vue
+++ resources/js/Pages/Liquijobs/Index.vue
@@ -6,6 +6,7 @@ import DangerButton from '@/Components/DangerButton.vue';
 import PrimaryLink from '@/Components/PrimaryLink.vue';
 import Pagination from '@/Components/Pagination.vue';
 import TextInput from '@/Components/TextInput.vue';
+import Dropdown from '@/Components/Dropdown.vue'
 
 import SearchBarSideBar from '@/Components/SearchBarSideBar.vue';
 import Calendar from '@/Components/Calendar.vue';
@@ -74,6 +75,7 @@ const deleteTrade = (id) => {
 				                    <div style="display: none;" class="shrink-0 flex items-center"><a href="http://127.0.0.1:8000/liquijobs"><img src="/images/logos/liquis-logo.png" alt="LiquiTraq" class="block h-9 w-auto"></a></div>
 				                </div>
 
+
 				                <div class="mx-auto relative flex items-center gap-6 lg:items-end">
 				                    <div class="relative flex items-center gap-6 lg:items-end">
 				                        <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" class="max-w-2xl max-h-2xl rounded-full cursor-pointer" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="User dropdown">
@@ -104,6 +106,54 @@ const deleteTrade = (id) => {
 				                        </div>
 				                    </div>
 				                </div>
+				                <div class="mx-auto relative flex items-center gap-6 lg:items-end">
+				                
+				                    <!-- <div class="relative flex items-center gap-6 lg:items-end">
+				                        <h1>MY ACCOUNT 	&dArr;</h1>
+				                    </div> -->
+				                    <Dropdown align="right" width="48">
+                                    <template #trigger>
+                                        <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                            <img class="size-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url" :alt="$page.props.auth.user.name">
+                                        </button>
+
+                                        <span v-else class="inline-flex rounded-md">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                                {{ $page.props.auth.user.name }}
+
+                                                <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </template>
+
+                                    <template #content>
+                                        <!-- Account Management -->
+                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                            Manage Account
+                                        </div>
+
+                                        <DropdownLink :href="route('profile.show')">
+                                            Profile
+                                        </DropdownLink>
+
+                                        <!-- <DropdownLink v-if="$page.props.jetstream.hasApiFeatures" :href="route('api-tokens.index')">
+                                            API Tokens
+                                        </DropdownLink> -->
+
+                                        <div class="border-t border-gray-200 dark:border-gray-600" />
+
+                                        <!-- Authentication -->
+                                        <form @submit.prevent="logout">
+                                            <DropdownLink as="button">
+                                                Log Out
+                                            </DropdownLink>
+                                        </form>
+                                    </template>
+                                </Dropdown>
+				                    
+				                </div>
 
 				                <!-- <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
 				                    <input type="search" name="search" placeholder="Search anything" />
@@ -113,7 +163,6 @@ const deleteTrade = (id) => {
 
 				                <!-- <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="search" name="search" placeholder="Search anything" /> -->
 				                <br/>
-
 
 				                 <h2 class="block w-full text-center">FILTER JOBS BY</h2>
 				                 <select class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
