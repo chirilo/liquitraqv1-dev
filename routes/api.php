db308102fb9df79ec015c84f035fcb34b0cc0dfe
Chi Rilo <cbenolirao@ibsportal.com.au>
2025-07-19 21:24:48 +0300

    add events API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\EventController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// routes/api.php
+Route::get('/events', [EventController::class, 'index']);
